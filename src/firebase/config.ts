eb6ea7345d12233d6f6046fc850db0858b8fd3c5
ege.durmaz <ege.durmaz@trendyol.com>
2026-01-06 01:20:13 +0300

    feat: VR-01 added voice isolation

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "***REMOVED***",
-  "appId": "***REMOVED***",
-  "apiKey": "***REMOVED***",
-  "authDomain": "***REMOVED***.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "***REMOVED***"
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET
 };
