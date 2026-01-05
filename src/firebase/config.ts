5e48ad977a07b55d31ec34f2fcb9d77e20d3b879
ege.durmaz <ege.durmaz@trendyol.com>
2026-01-05 18:05:14 +0300

    feat: VR-04 .

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,7 @@
 export const firebaseConfig = {
-  "projectId": "studio-538325307-b0443",
-  "appId": "1:297447175760:web:e89cab0a514f31af9dc355",
-  "apiKey": "AIzaSyD7Ce3gs49IGzs-Lx41BdrZRe4nhEryze0",
-  "authDomain": "studio-538325307-b0443.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "297447175760"
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
 };
