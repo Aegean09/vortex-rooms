a06d8db401186aa68156aa224b9164c9594e2ff5
Aegean09 <aegeandurmaz1@gmail.com>
2026-02-18 23:15:45 +0300

    feat: VR-20 CSP improvements have been made

--- next.config.ts
+++ next.config.ts
@@ -50,10 +50,10 @@ const nextConfig: NextConfig = {
             key: 'Content-Security-Policy',
             value: [
               "default-src 'self'",
-              "script-src 'self' 'unsafe-inline' 'unsafe-eval'",
-              "style-src 'self' 'unsafe-inline'",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdnjs.buymeacoffee.com",
+              "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
               "img-src 'self' data: https: blob:",
-              "font-src 'self' data:",
+              "font-src 'self' data: https://fonts.gstatic.com",
               "connect-src 'self' https: wss: *.googleapis.com *.google.com *.firebaseio.com *.firebase.com",
               "frame-ancestors 'none'",
               "base-uri 'self'",
